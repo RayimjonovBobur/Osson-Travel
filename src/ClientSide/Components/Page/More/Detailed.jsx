5e8237ec3416332a82f3d66412c3b3d7ad3babce
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-06-22 14:18:50 +0500

    dffd

--- src/ClientSide/Components/Page/More/Detailed.jsx
+++ src/ClientSide/Components/Page/More/Detailed.jsx
@@ -3,7 +3,10 @@ import "./Detailed.scss";
 import dubai from "../../../assets/Images/dubai.jpg";
 import { DetaileDate } from "./DetailedData";
 import { useDispatch } from "react-redux";
-import hours_icon from "../../../assets/Images/24-hours.png"
+import hours_icon from "../../../assets/Images/24-hours.png";
+import { GoCalendar } from "react-icons/go";
+import { Ri24HoursFill } from "react-icons/ri";
+import { IoManSharp } from "react-icons/io5";
 
 const Detailed = () => {
   const [isactive, setActive] = useState(false);
@@ -35,7 +38,7 @@ const Detailed = () => {
                 mehmonxonalari
               </h3>
             </div>
-          </div>  
+          </div>
         </div>
       </div>
       <div className="detailed-content">
@@ -58,13 +61,84 @@ const Detailed = () => {
               <div className="tab-pane active">
                 <div className="tours-tabs__info">
                   <div className="tours-tabs__info__item">
-                    <div className="tours-tabs__info__item__content">
+                    <div className="tours-tabs__info__item__content d-flex">
                       <div className="tours-tabs__info__item__icon">
-                        <img src={hours_icon} alt="" />
+                        <Ri24HoursFill />
                       </div>
-                      <h5 className="tours-tabs__info__item__title"></h5>
+                      <div className="mx-4">
+                        <div className="tours-tabs__info__item__title">
+                          8 kun 7 kecha
+                        </div>
+                        <div className="tours-tabs__info__item__description">
+                          Kunlar soni
+                        </div>
+                      </div>
+                    </div>
+                  </div>{" "}
+                  <div className="tours-tabs__info__item">
+                    <div className="tours-tabs__info__item__content d-flex">
+                      <div className="tours-tabs__info__item__icon">
+                        <IoManSharp />
+                      </div>
+                      <div className="mx-4">
+                        <div className="tours-tabs__info__item__title">0+ </div>
+                        <div className="tours-tabs__info__item__description">
+                          Yoshi
+                        </div>
+                      </div>
+                    </div>
+                  </div>{" "}
+                  <div className="tours-tabs__info__item">
+                    <div className="tours-tabs__info__item__content d-flex">
+                      <div className="tours-tabs__info__item__icon">
+                        <GoCalendar />
+                      </div>
+                      <div className="mx-4">
+                        <div className="tours-tabs__info__item__title">
+                          Aprel – Oktyabr
+                        </div>
+                        <div className="tours-tabs__info__item__description">
+                          Mavsum
+                        </div>
+                      </div>
                     </div>
                   </div>
+                </div>
+                <div className="tours-tabs__content padding-all">
+                  <h2>
+                    <strong>Manzali sayohat</strong>
+                  </h2>
+                  <p>
+                    <span></span>
+                  </p>
+                  <table
+                    style={{ height: "233px", width: "844px" }}
+                    className="table table-bordered tours-tabs__table"
+                  >
+                    <tbody>
+                      <tr>
+                        <td style={{ width: "184px" }}>
+                          <strong>JO‘NAB KETISH</strong>
+                        </td>
+                        <td style={{ width: "660px" }}>Seshanba, Juma</td>
+                      </tr>
+                      <tr>
+                        <td style={{ width: "184px" }}>
+                          <strong>QAYTIB KELISH</strong>
+                        </td>
+                        <td style={{ width: "660px" }}>Seshanba, Juma</td>
+                      </tr>{" "}
+                      <tr>
+                        <td style={{ width: "184px" }}>
+                          <strong>VIZA</strong>
+                        </td>
+                        <td style={{ width: "660px" }}>
+                          Talab qilinmaydi, xorijga chiqish pasporti, uchib
+                          ketishdan 72 soat oldin qilingan PZR test zarur
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
